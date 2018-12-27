e2832697fcd9900bec84827669192ffe3f6ff350
sachinrokade <srokade918@gmail.com>
2018-12-27 07:10:53 -0800

    git Demo

---
+++ src/main/java/com/example/consume/TestConsume.java
@@ -0,0 +1,30 @@
+package com.example.consume;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.example.model.Product;
+
+public class TestConsume {
+
+	private static 	RestTemplate restTemplate=new RestTemplate();
+	
+	public static void main(String[] args)
+	{
+		//addProduct(new Product(1,"sdfds"));
+		getByID(5);
+	}
+
+	private static void getByID(int i) {
+		
+		Product p=restTemplate.getForObject("http://localhost:9094/get/"+i,Product.class);
+		System.out.println("GET Cumsume  "+p);
+	
+		
+	}
+
+	/*private static void addProduct(Product product) 
+	{
+		Product result = restTemplate.postForObject("http://localhost:9094/add",product,Product.class);
+		System.out.println("Counsume POST "+result);
+	}*/
+}
